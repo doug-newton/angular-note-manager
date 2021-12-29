d1a7232dade5558a57f6ab47857b960320a64430
Doug Newton <27059467+doug-newton@users.noreply.github.com>
2021-12-29 09:30:56 +0200

    start notes component

--- src/app/notes/notes.component.ts
+++ src/app/notes/notes.component.ts
@@ -1,11 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Note {
+  _id: string
+  title: string
+  body: string
+}
+
 @Component({
   selector: 'app-notes',
   templateUrl: './notes.component.html',
   styleUrls: ['./notes.component.scss']
 })
 export class NotesComponent implements OnInit {
+
+  notes: Note[] = [
+    {_id: 'fasdfasd', title: 'hello world', body: 'this is a random note'},
+    {_id: 'fasdfase', title: 'hello world again', body: 'this is another random note'},
+  ]
 
   constructor() { }
 
