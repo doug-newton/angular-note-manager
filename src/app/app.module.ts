4bc83ddb2fbc79276d8c5313b696085cdf0e1ebd
Doug Newton <27059467+doug-newton@users.noreply.github.com>
2021-12-29 09:24:15 +0200

    add and nav to home and notes components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { NotesComponent } from './notes/notes.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    NotesComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
